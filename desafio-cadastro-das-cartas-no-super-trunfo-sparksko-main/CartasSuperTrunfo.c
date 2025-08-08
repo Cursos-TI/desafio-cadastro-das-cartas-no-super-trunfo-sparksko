9d4ffaec7edff7d7cd9d4360fea7fc3c28308d0e
Raphael Rocha <sparks.ph@gmail.com>
2025-08-08 15:49:37 -0300

    adicionei a linha de densidade populacional e pib per capita

--- desafio-cadastro-das-cartas-no-super-trunfo-sparksko-main/CartasSuperTrunfo.c
+++ desafio-cadastro-das-cartas-no-super-trunfo-sparksko-main/CartasSuperTrunfo.c
@@ -26,6 +26,8 @@ int main()
     float area1, area2;                       // Area da cidade em km²
     float pib1, pib2;                         // PIB da cidade
     int pontosTuristicos1, pontosTuristicos2; // numero de pontos turisticos
+    float densidade1, densidade2;             // densidade populacional
+    float pibpercapita1, pibpercapita2;       // PIB per capita
 
     // Apresentação do jogo e das cartas
     printf("Super Trunfo \n");
@@ -81,18 +83,28 @@ int main()
     printf("Digite o número de pontos turisticos \n");
     scanf("%d", &pontosTuristicos2);
 
+//Calcular os valores da densidade e pib per capita antes da exibição
+
+       densidade1 = populacao1 / area1;
+       pibpercapita1 = (pib1 * 1000000000) / populacao1;
+
+       densidade2 = populacao2 / area2;
+       pibpercapita2 = (pib2 * 1000000000) / populacao2;
+
+
     // Exibir os dados das cartas cadastradas
 
     printf("\nCartas cadastradas com sucesso!\n");
 
-    printf("\nCarta 1:\nEstado: %c\nCódigo: %s\nCidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões\nPontos Turísticos: %d\n",
-           estado1, codigo1, nomeCidade1, populacao1, area1, pib1, pontosTuristicos1);
+   printf("\nCarta 1:\nEstado: %c\nCódigo: %s\nCidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões\nPontos Turísticos: %d\nDensidade Populacional:%.2f hab/km²\nPIB per Capita:%.2f reais",
+           estado1, codigo1, nomeCidade1, populacao1, area1, pib1, pontosTuristicos1, densidade1, pibpercapita1);
 
-    printf("\nCarta 2:\nEstado: %c\nCódigo: %s\nCidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões\nPontos Turísticos: %d\n",
-           estado2, codigo2, nomeCidade2, populacao2, area2, pib2, pontosTuristicos2);
+    printf("\nCarta 2:\nEstado: %c\nCódigo: %s\nCidade: %s\nPopulação: %d\nÁrea: %.2f km²\nPIB: %.2f bilhões\nPontos Turísticos: %d\nDensidade Populacional:%.2f hab/km²\nPIB per Capita:%.2f reais",
+           estado2, codigo2, nomeCidade2, populacao2, area2, pib2, pontosTuristicos2, densidade2, pibpercapita2);
 
-    printf("Pressione qualquer tecla para sair...\n");
+    printf("\nPressione qualquer tecla para sair...\n");
+    
     scanf(" %c"); // Espera por uma tecla para encerrar o programa
 
     return 0;
-}
+} 
